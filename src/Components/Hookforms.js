7e829a15bf669569ab620bf8fb7748638d4e512e
Ruwanthi Prasadini <you@example.com>
2020-11-23 23:53:10 +0530

    userstatus corrected

--- src/Components/Hookforms.js
+++ src/Components/Hookforms.js
@@ -5,42 +5,36 @@ import { useForm} from "react-hook-form";
 export default function App() {
 
     const [user,setUser]=useState([]);
+
+    const[data,setData]=useState({fname:'',jobState:'',comment:'',sport1:'',sport2:'',sport3:'',gender:''})
+    
+    const addUser=(e)=>{ 
+        e.preventDefault();
+        setUser([...user,{fname:data.fname,jobState:data.jobState,comment:data.comment,sport1:data.sport1,sport2:data.sport2,sport3:data.sport3,gender:data.gender}]) 
+        
+    }
+
     const { register, handleSubmit } = useForm();
     
     const onSubmit = data=> {
         var temp=(JSON.stringify(data));
         var obj = JSON.parse(temp);
-    
-    
-    Let newUser[]=user;
-    
-    newUser.push([{
-    name:obj.fname,
-    Jobrole:obj.jobstate,
-    comment:obj.comments,
-    sport1:obj.sport1,
-    sport2:obj.sport2,
-    sport3:obj.sport3,
-    gender:obj.gender
-    }])
-
     }
-
     
     return (
         <React.Fragment>
 
-            <button>Add new User</button>
+            
             <form onSubmit={handleSubmit(onSubmit)}>
 
                 <label for="fname">Name:</label>
-                <input type="name" id="name" name="fname" placeholder="Name" value="{user.fname}" ref={register}
-                onChange={e =>setUser({...user,e.target.value})} />
+                <input type="name" id="fname" name="fname" placeholder="Name" value={data.fname} ref={register}
+                onChange={e =>setData({...data,fname:e.target.value})} />
                 <br/>
                 <br/>
 
-                <label for="inputstate">Job Role:</label>
-                <select id="inputState" ref={register} name="jobstate" value="{user.jobstatus}"onChange={e =>setUser({...user,e.target.value})} >
+                <label for="jobState">JobState:</label>
+                <select id="jobState" ref={register} name="jobState" value= {data.jobState} onChange={e =>setData({...data,jobState:e.target.value})} >
                     
                     <option>Student</option>
                     <option>Teacher</option>
@@ -51,23 +45,23 @@ export default function App() {
             
 
                 <label for="Comment">Comment:</label>
-                <textarea id="comment" name="comments" ref={register} value="{user.comments}"onChange={e =>setUser({...user,e.target.value})} ></textarea>
+                <textarea id="comment" name="comment" ref={register} value={data.comment}onChange={e =>setData({...data,comment:e.target.value})} ></textarea>
                 <br/>
                 <br/>
 
                 <label for="checkbox">Sports:</label>
-                <input type="checkbox" id="gridCheck1" ref={register} name="sport1" value="{user.sport1}"
-                onChange={e =>setUser({...user,e.target.value})} />
+                <input type="checkbox" id="cricket" ref={register} name="sport1" value={data.sport1}
+                onChange={e =>setData({...data,sport1:e.target.id})} />
                 <label for="gridCheck">
                     Cricket
                 </label>
-                <input type="checkbox" id="gridCheck1" ref={register} name="sport2" value="{user.sport2}" 
-                onChange={e =>setUser({...user,e.target.value})} />
+                <input type="checkbox" id="vollyball" ref={register} name="sport2" value={data.sport2}
+                onChange={e =>setData({...data,sport2:e.target.id})} />
                 <label for="gridCheck">
                     Volly Ball
                 </label>
-                <input type="checkbox" id="gridCheck2" ref={register} name="sport3"value="{user.sport3}" 
-                onChange={e =>setUser({...user,e.target.value})} />
+                <input type="checkbox" id="hocky" ref={register} name="sport3"value={data.sport3} 
+                onChange={e =>setData({...data,sport3:e.target.id})} />
                 <label for="gridCheck">
                     Hocky
                 </label>
@@ -76,46 +70,61 @@ export default function App() {
                 
                 <label for="Radio">Gender:</label>
                 <label for="Radio">Male</label>
-                <input type="radio" id="male" name="gender" value="male" ref={register} value="{user.gender}"
-                onChange={e =>setUser({...user,e.target.value})} ></input>
+                <input type="radio" id="male" name="gender" ref={register} value={data.gender}
+                onChange={e =>setData({...data,gender:e.target.id})} ></input>
                 <label for="Radio">Female</label>
-                <input type="radio" id="female" name="gender" value="female" ref={register} value="{user.gender}"
-                onChange={e =>setUser({...user,e.target.value})} ></input>
+                <input type="radio" id="female" name="gender" ref={register} value={data.gender}
+                onChange={e =>setData({...data,gender:e.target.id})} ></input>
                 <br/>
                 <br/>
                 
-                <button type="submit">Add</button>
+                <button onClick={addUser}>Add</button>
 
             </form>
             
             
             <div>
-            {user.map((newUser[]) => {
-                <table>
-                        <th>Name</th>
-                        <th>JobRole</th>
-                        <th>Commment</th>
-                        <th>Sport1</th>
-                        <th>Sport2</th>
-                        <th>Sport3</th>
-                        <th>Gender</th>
-                        
+            
+            <table border="2">
+                <tr>
+                    <th>Name</th>
+                    <th>JobRole</th>
+                    <th>Comment</th>
+                    <th>Sport1</th>
+                    <th>Sport2</th>
+                    <th>Sport3</th>
+                    <th>Gender</th>
+                </tr>
+                    {
+                        user.map(newUser => (
                         <tr>
-                        <td>{newUser[].name}</td>
-                        <td>{newUser[].jobrole}</td>
-                        <td>{newUser[].sport1}</td>
-                        <td>{newUser[].sport2}</td>
-                        <td>{newUser[].sport3}</td>
-                        <td>{newUser[].gender}</td>
+                            <td>{newUser.fname}</td>
+                            <td>{newUser.jobState}</td>
+                            <td>{newUser.comment}</td>
+                            <td>{newUser.sport1}</td>
+                            <td>{newUser.sport2}</td>
+                            <td>{newUser.sport3}</td>
+                            <td>{newUser.gender}</td>
                         </tr>
-                    </table>
-            }
+                        ))
+                    } 
+        
+            </table>
+            <h4>Your Name is = { data.fname}</h4>
+            <h4>Your Job Role is = { data.jobState}</h4>
+            <h4>Your comment is = { data.comment}</h4>
+            <h4>Your Sport1 is = { data.sport1}</h4>
+            <h4>Your Sport2 is = { data.sport2}</h4>
+            <h4>Your Sport3 is = { data.sport3}</h4>
+            <h4> Gender  =  { data.gender}</h4> 
+            
+
+            
             </div>
 
         </React.Fragment>
     
     );
-    
-    
 
-}
+                }
+
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Hookforms from './Components/Hookforms';
-// import Hookforms from './Components/Hookforms';
 
 ReactDOM.render(
   <React.StrictMode>
+   
     <Hookforms/>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -16,3 +17,12 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+
+
+
+
+
